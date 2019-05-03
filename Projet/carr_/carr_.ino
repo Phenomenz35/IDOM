6bf20dc614986e14aa59c5ad5ce80ecea1044868
Ogustinho <augustin.lelouer@gmail.com>
2019-05-03 13:07:39 +0200

    void setup

--- Projet/carr_/carr_.ino
+++ Projet/carr_/carr_.ino
@@ -60,8 +60,7 @@ void setup() {
   TCCR2B=TCCR2B&0xf8|0x01;    // Pin3,Pin11 PWM 31250Hz
     
   Omni.PIDEnable(0.31,0.01,0,10);
+  Omni.carre(30,1500,500,false);
 }
 void loop() {
-  Omni.carre(30,1500,500,false);
-  while(1);
 }
